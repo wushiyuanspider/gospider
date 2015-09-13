db728b0df698996735678b1bdd1205acf78c12d0
buchenglei <mybc724@163.com>
2015-09-13 14:50:12 +0800

    重新设计了配置文件的逻辑

--- src/loadconf.go
+++ src/loadconf.go
@@ -11,8 +11,8 @@ type Spider struct {
 	name           string
 	startURL       string
 	depth          int
-	urls           []*regexp.Regexp
-	contents       []*regexp.Regexp
+	urls           map[string]*regexp.Regexp
+	contents       map[string]map[string]*regexp.Regexp
 	matchedURL     int
 	matchedContent int
 	startTime      time.Time
@@ -33,7 +33,7 @@ func NewSpider(filepath string) (*Spider, error) {
         return nil, err
     }
     
-	spider.startURL, err = conf.GetValue("core", "url")
+	spider.startURL, err = conf.GetValue("core", "startURL")
     if err != nil {
         return nil, err
     }
@@ -44,42 +44,43 @@ func NewSpider(filepath string) (*Spider, error) {
     }
 
 	// 获得待匹配URL的正则表达式列表
-	urls, err := conf.GetSection("urls")
+	urls, err := conf.GetSection("url")
 	if err != nil {
 		return nil, err
 	}
 
-    // 获得待匹配内容的正则表达式列表
-	contents, err := conf.GetSection("contents")
-	if err != nil {
-		return nil, err
-	}
-
-    // 编译用于匹配URL的正则表达式
-	spider.urls = make([]*regexp.Regexp, len(urls))
-	for _, tmp := range urls {
-		re, err := regexp.Compile(tmp)
+    // 初始化外层的map
+	spider.urls = make(map[string]*regexp.Regexp, len(urls))
+	spider.contents = make(map[string]map[string]*regexp.Regexp, len(urls))
+	
+	// 编译用于匹配URL的正则表达式
+	for k, v := range urls {
+		re_url, err := regexp.Compile(v)
 		if err != nil {
-			fmt.Println("Compile ", tmp, " error: ", err)
+			fmt.Println("Compile ", v, " error: ", err)
 		} else {
-			spider.urls = append(spider.urls, re)
+			// 将URL保存，并在contents中创建对应的存储map
+			spider.urls[k] = re_url
+			// 获取每个URL对应的待匹配的正则表达式
+			sub, err := conf.GetSection("url." + k)
+			if err != nil {
+				return nil, fmt.Errorf("Don't find url." + k)
+			}
+			// 初始化里层的map
+			spider.contents[k] = make(map[string]*regexp.Regexp, len(k))
+			for x, y := range sub {
+				re_con, err := regexp.Compile(y)
+				if err != nil {
+					fmt.Println("Compile ", y, "error:", err)
+				} else {
+					spider.contents[k][x] = re_con
+				}
+			} 
 		}
 	}
 
-    // 编译用于匹配页面内容的正则表达式
-	spider.contents = make([]*regexp.Regexp, len(contents))
-	for _, tmp := range contents {
-		re, err := regexp.Compile(tmp)
-		if err != nil {
-			fmt.Println("Compile ", tmp, " error: ", err)
-		} else {
-			spider.contents = append(spider.contents, re)
-		}
-	}
-
-	// 检查用于匹配内容的slice是否为空
 	// 为空则什么也匹配不到，没有意义
-	if len(spider.contents) == 0 {
+	if len(spider.urls) == 0 {
 		return nil, fmt.Errorf("Nothing to fetch!")
 	}
 
